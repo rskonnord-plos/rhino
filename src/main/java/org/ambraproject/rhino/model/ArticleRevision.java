2319835e365cd55a2b3f38983294a16c7bb1eca8
Ryan Skonnord <rskonnord@plos.org>
2015-03-19 15:00:35 -0700

    DPRO-511: Early doodle of ArticleRevision table

---
+++ src/main/java/org/ambraproject/rhino/model/ArticleRevision.java
@@ -0,0 +1,58 @@
+package org.ambraproject.rhino.model;
+
+import org.ambraproject.models.AmbraEntity;
+
+import java.util.UUID;
+
+public class ArticleRevision extends AmbraEntity {
+
+  private String doi;
+  private int revisionNumber;
+  private UUID crepoUuid;
+
+  public String getDoi() {
+    return doi;
+  }
+
+  public void setDoi(String doi) {
+    this.doi = doi;
+  }
+
+  public int getRevisionNumber() {
+    return revisionNumber;
+  }
+
+  public void setRevisionNumber(int revisionNumber) {
+    this.revisionNumber = revisionNumber;
+  }
+
+  public UUID getCrepoUuid() {
+    return crepoUuid;
+  }
+
+  public void setCrepoUuid(UUID crepoUuid) {
+    this.crepoUuid = crepoUuid;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ArticleRevision that = (ArticleRevision) o;
+
+    if (revisionNumber != that.revisionNumber) return false;
+    if (crepoUuid != null ? !crepoUuid.equals(that.crepoUuid) : that.crepoUuid != null) return false;
+    if (doi != null ? !doi.equals(that.doi) : that.doi != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = doi != null ? doi.hashCode() : 0;
+    result = 31 * result + revisionNumber;
+    result = 31 * result + (crepoUuid != null ? crepoUuid.hashCode() : 0);
+    return result;
+  }
+}
