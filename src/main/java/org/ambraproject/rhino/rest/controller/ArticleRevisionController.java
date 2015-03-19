49392a27a4e410f9d7d6c8db41c2756e86da2229
Ryan Skonnord <rskonnord@plos.org>
2015-03-19 16:25:15 -0700

    Sketch of actually querying for ArticleRevision

---
+++ src/main/java/org/ambraproject/rhino/rest/controller/ArticleRevisionController.java
@@ -0,0 +1,89 @@
+package org.ambraproject.rhino.rest.controller;
+
+import org.ambraproject.rhino.model.ArticleRevision;
+import org.ambraproject.rhino.rest.RestClientException;
+import org.ambraproject.rhino.rest.controller.abstr.RestController;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.UUID;
+
+@Controller
+public class ArticleRevisionController extends RestController {
+
+  @Autowired
+  private HibernateTemplate hibernateTemplate;
+
+  @RequestMapping(value = "articles/revisions/create", method = RequestMethod.GET)
+  public void createRevision(HttpServletRequest request, HttpServletResponse response,
+                             @RequestParam(value = "id", required = true) String doi,
+                             @RequestParam(value = "r", required = true) Integer revisionNumber)
+      throws IOException {
+
+    // Just a dumb placeholder. TODO: Remove
+    // This violates the idempotence of GET, obviously.
+
+    ArticleRevision r = new ArticleRevision();
+    r.setDoi(doi);
+    r.setRevisionNumber(revisionNumber);
+    r.setCrepoUuid(UUID.randomUUID().toString());
+    hibernateTemplate.persist(r);
+  }
+
+
+  @Transactional(readOnly = true)
+  @RequestMapping(value = "articles/revision", method = RequestMethod.GET)
+  public void findRevision(HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam(value = "id", required = true) final String doi,
+                           @RequestParam(value = "r", required = false) final Integer revisionNumber)
+      throws IOException {
+
+    // TODO: Pull into service class
+    // For prototype, just using this method as a development sandbox
+
+    String uuid;
+    if (revisionNumber == null) {
+      uuid = hibernateTemplate.execute(new HibernateCallback<String>() {
+        @Override
+        public String doInHibernate(Session session) throws HibernateException, SQLException {
+          Query query = session.createQuery("select crepoUuid from ArticleRevision where doi=? order by revisionNumber desc");
+          query.setString(0, doi);
+          query.setMaxResults(1);
+          return (String) DataAccessUtils.singleResult(query.list());
+        }
+      });
+    } else {
+      uuid = (String) DataAccessUtils.uniqueResult(
+          hibernateTemplate.find("select crepoUuid from ArticleRevision where doi=? and revisionNumber=?",
+              doi, revisionNumber));
+    }
+    if (uuid == null) {
+      StringBuilder message = new StringBuilder().append("No revision exists with id=").append(doi);
+      if (revisionNumber != null) {
+        message.append(" and r=").append(revisionNumber);
+      }
+      throw new RestClientException(message.toString(), HttpStatus.NOT_FOUND);
+    }
+
+    try (PrintWriter writer = response.getWriter()) {
+      writer.println(uuid);
+    }
+  }
+
+}
