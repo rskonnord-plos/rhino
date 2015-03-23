be37f1fc0c0214bbc733c86a3572c915898b7072
Ryan Skonnord <rskonnord@plos.org>
2015-03-23 12:44:41 -0700

    Implement userMetadata creation

--- src/main/java/org/ambraproject/rhino/service/ArticleRevisionService.java
+++ src/main/java/org/ambraproject/rhino/service/ArticleRevisionService.java
@@ -3,6 +3,7 @@ package org.ambraproject.rhino.service;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.ByteStreams;
+import com.google.gson.Gson;
 import org.ambraproject.models.Article;
 import org.ambraproject.rhino.content.xml.ArticleXml;
 import org.ambraproject.rhino.content.xml.ManifestXml;
@@ -27,9 +28,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -38,6 +40,8 @@ public class ArticleRevisionService extends AmbraService {
 
   @Autowired
   private ContentRepoService versionedContentRepoService;
+  @Autowired
+  private Gson crepoGson;
 
   public void ingest(InputStream archiveStream) throws IOException, XmlContentException {
     String prefix = "ingest_" + new Date().getTime() + "_";
@@ -111,46 +115,85 @@ public class ArticleRevisionService extends AmbraService {
       articleMetadata.setDoi(articleIdentity.getKey()); // Should ArticleXml.build do this itself?
     }
 
-    Collection<RepoObject> toUpload = new ArrayList<>(files.size());
+    Map<String, RepoObject> toUpload = new LinkedHashMap<>(); // keys are zip entry names
 
     RepoObject manifestObject = new RepoObject.RepoObjectBuilder("manuscript/" + articleIdentity)
         .fileContent(manuscriptFile)
         .contentType(MediaType.APPLICATION_XML)
         .downloadName(articleIdentity.forXmlAsset().getFileName())
         .build();
-    toUpload.add(manifestObject);
+    toUpload.put(manuscriptRepr.getEntry(), manifestObject);
 
     for (ManifestXml.Asset asset : assets) {
       for (ManifestXml.Representation representation : asset.getRepresentations()) {
-        File file = files.get(representation.getEntry());
+        String entry = representation.getEntry();
+        File file = files.get(entry);
         if (file.equals(manuscriptFile)) continue;
         String key = representation.getName() + "/" + AssetIdentity.create(asset.getUri());
         RepoObject repoObject = new RepoObject.RepoObjectBuilder(key)
             .fileContent(file)
                 // TODO Add more metadata. Extract from articleMetadata and manifestXml as necessary.
             .build();
-        toUpload.add(repoObject);
+        toUpload.put(entry, repoObject);
       }
     }
 
     // Post files
-    Collection<RepoVersion> created = new ArrayList<>(toUpload.size());
-    for (RepoObject repoObject : toUpload) { // Excellent candidate for parallelization! I can haz JDK8 plz?
+    Map<String, RepoVersion> created = new LinkedHashMap<>();
+    for (Map.Entry<String, RepoObject> entry : toUpload.entrySet()) { // Excellent candidate for parallelization! I can haz JDK8 plz?
+      RepoObject repoObject = entry.getValue();
       RepoObjectMetadata createdMetadata = versionedContentRepoService.autoCreateRepoObject(repoObject);
-      created.add(createdMetadata.getVersion());
+      created.put(entry.getKey(), createdMetadata.getVersion());
     }
 
-    Object userMetadataForCollection = null; // TODO Implement
+    Map<String, Object> userMetadataForCollection = buildArticleAsUserMetadata(manifestXml, created);
 
     // Create collection
     RepoCollection collection = RepoCollection.builder()
         .setKey(articleIdentity.toString())
-        .setObjects(created)
-            // TODO Set user metadata (needs lib support)
+        .setObjects(created.values())
+        .setUserMetadata(crepoGson.toJson(userMetadataForCollection))
         .build();
     RepoCollectionMetadata collectionMetadata = versionedContentRepoService.autoCreateCollection(collection);
 
     return collectionMetadata;
+  }
+
+  private Map<String, Object> buildArticleAsUserMetadata(ManifestXml manifestXml, Map<String, RepoVersion> objects) {
+    Map<String, Object> map = new LinkedHashMap<>();
+    map.put("format", "nlm");
+
+    String manuscriptKey = manifestXml.getArticleXml();
+    map.put("manuscript", new RepoVersionRepr(objects.get(manuscriptKey)));
+
+    List<ManifestXml.Asset> assetSpec = manifestXml.parse();
+    List<Map<String,Object>> assetList = new ArrayList<>(assetSpec.size());
+    for (ManifestXml.Asset asset : assetSpec) {
+      Map<String,Object> assetMetadata=new LinkedHashMap<>();
+      assetMetadata.put("doi",AssetIdentity.create(asset.getUri()).toString());
+
+      Map<String,Object> assetObjects=new LinkedHashMap<>();
+      for (ManifestXml.Representation representation : asset.getRepresentations()) {
+        RepoVersion objectForRepr = objects.get(representation.getEntry());
+        assetObjects.put(representation.getName(), new RepoVersionRepr(objectForRepr));
+      }
+      assetMetadata.put("objects", assetObjects);
+
+      assetList.add(assetMetadata);
+    }
+    map.put("assets", assetList);
+
+    return map;
+  }
+
+  private static class RepoVersionRepr {
+    private final String key;
+    private final String uuid;
+
+    private RepoVersionRepr(RepoVersion repoVersion) {
+      this.key = repoVersion.getKey();
+      this.uuid = repoVersion.getUuid().toString();
+    }
   }
 
   private void writeRevision(RepoCollectionMetadata articleCollection) {
